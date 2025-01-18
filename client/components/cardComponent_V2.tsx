44d5aec423ff6e49e2d1c8e4fde43722e93f3f53
salvatoresalerno <salvadj2012@gmail.com>
2025-01-18 12:14:49 +0100

    add images placeholder

--- client/components/cardComponent_V2.tsx
+++ client/components/cardComponent_V2.tsx
@@ -136,7 +136,7 @@ const CardComponent_V2 = ({veicolo, user}: CardProps) => {
                         fill
                         sizes="33vw"
                         priority
-                        onError={() => veicolo.tipo === TipoVeicolo.AUTO ? setSrc("veicoli/auto" +  randomNumber() + ".jpg") : setSrc("veicoli/moto" +  randomNumber() + ".jpg")}                        
+                        onError={() => veicolo.tipo === TipoVeicolo.AUTO ? setSrc("placeholder/auto" +  randomNumber() + ".jpg") : setSrc("placeholder/moto" +  randomNumber() + ".jpg")}                        
                     />
                 </div>
                 <div className="flex-1">
